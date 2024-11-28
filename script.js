3962c08d7d01e6b9a0c1fe5e3a985ee21745648e
stefanutanh <bergs83@gmail.com>
2024-11-28 11:34:22 +0100

    update

---
+++ script.js
@@ -0,0 +1,14 @@
+// Variabel 1: Checkboxen
+const checkbox = document.querySelector('input[type="checkbox"]');
+console.log(checkbox); 
+
+// Variabel 2: Alla textfält
+const textfields = document.getElementsByClassName('textfield');
+console.log(textfields); 
+// Variabel 3: Knappen
+const button = document.getElementById('myButton');
+console.log(button);  
+// Variabel 4: Div-elementet
+
+const divElement = document.querySelector('#myDiv');
+console.log(divElement); 
