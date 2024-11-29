7b6885764b06f9767cf7c6c56c71967bc028c01a
stefanutanh <bergs83@gmail.com>
2024-11-29 13:14:46 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,32 @@
 
 
-// Uppgift 4: Skapa variabler för att hålla HTMLElement-objekt
-
-
-const checkbox = document.querySelector('input[type="checkbox"]');
-const textFields = document.getElementsByClassName('textfield');
+const checkbox = document.getElementById('checkbox');
+const textFields = document.querySelectorAll('.textfield');
 const button = document.getElementById('button');
 const contentDiv = document.querySelector('.content-div');
 
-// Uppgift 5: Skapa en fördefinierad funktion
+function handleInput(e) {
+    console.log(e);
+    if (e.target.name === 'content') {
+        contentDiv.innerHTML = e.target.value;
+    }
+}
+
+checkbox.addEventListener('change', function(){
+console.log("checkbox");
+for (let textfield of textFields) {
+    if (textfield.name === 'color') {
+        contentDiv.style.background = textfield.value;
+    }
+}
+});
+
+textFields.forEach((field) => field.addEventListener('input', handleInput));
+
+button.addEventListener('click', function() {
+    console.log("Removed element");
+    contentDiv.remove();
+});
+
+
 
